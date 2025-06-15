6d61630690731e0fed48f4632ddd6b0ff67a1548
SHAWON0c <ssiddikee111@gmail.com>
2025-06-15 12:59:48 +0600

    responsive monoChromic Screen

--- src/Pages/MonoChromicScreen/MonoChromicScreen.jsx
+++ src/Pages/MonoChromicScreen/MonoChromicScreen.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 const MonoChromicScreen = () => {
     return (
         <div className="relative w-full h-auto">
-            {/* This div will appear on top of the image */}
-            <div className="absolute top-20 left-[600px] z-10  text-white max-w-xl">
-                <h1 className="text-4xl font-bold mb-4 text-black">Monochrome back screen</h1>
-                <p className="text-lg text-black">
+            {/* Overlay text container */}
+            <div className="absolute z-10 text-white max-w-xl px-4 
+                            top-10  transform lg:-translate-x-3/4 
+                            sm:top-16 md:top-20 lg:left-2/3">
+                <h1 className="text-lg sm:text-4xl md:text-4xl font-bold mb-4 text-black text-center mx-16 sm:mx-0 lg:mx-0 md:text-left">
+  Monochrome back screen
+</h1>
+
+                <p className=" hidden sm:block text-base sm:text-2xl text-black text-center md:text-left">
                     Eye-friendly back screen allows you to control the device and see the status 
                     displayed on the main screen — even when it’s turned away from you.
                 </p>
@@ -14,7 +19,7 @@ const MonoChromicScreen = () => {
 
             {/* Background image */}
             <img
-                className="w-full h-auto"
+                className="w-full h-auto transform  transition-transform duration-500"
                 src="/src/assets/MonoChromicScreen.svg"
                 alt="Monochrome Screen"
             />
